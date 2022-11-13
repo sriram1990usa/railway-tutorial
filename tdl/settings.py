3771d5145488431e0ba4c6ef81e52f049f87795b
sriram <sriram1990usa@gmail.com>
2022-11-12 19:51:11 -0800

    first commit

--- tdl/settings.py
+++ tdl/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-cvx0*axw#c$v2zof0(f5fc^4z(-a8n%$_fxvmc^0g+mj45ln(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-6882.up.railway.app/', '*']
+#ALLOWED_HOSTS = ['https://web-production-6882.up.railway.app/', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
