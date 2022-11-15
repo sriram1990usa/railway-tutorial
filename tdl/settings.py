93fcc1baea770e831a75c9a7037276410e18ca42
sriram <sriram1990usa@gmail.com>
2022-11-15 12:26:48 -0800

    changed settings.py

--- tdl/settings.py
+++ tdl/settings.py
@@ -53,17 +53,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tdl.wsgi.application'
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -74,7 +71,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 AUTH_PASSWORD_VALIDATORS = [
     { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
